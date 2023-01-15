fd5497879802a41b37bcc951a22d032e65792de3
Arif Shahriyar Nader <arif.nader95@gmail.com>
2023-01-16 03:07:32 +0600

    first commit

---
+++ api/routes/hotels.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import { countByCity, createHotel, deleteHotel, getHotel, getHotels, updateHotel } from '../controllers/hotel.js';
+
+import { createError } from '../utils/error.js';
+import { verifyAdmin } from '../utils/verifyToken.js';
+const router=express.Router();
+
+//Create
+router.post("/", verifyAdmin, createHotel);
+
+//Update
+router.put("/:id", verifyAdmin, updateHotel);
+
+//Delete
+router.delete("/:id", verifyAdmin, deleteHotel);
+
+//GET
+router.get("/find/:id", getHotel);
+
+//GET ALL
+router.get("/", getHotels);
+router.get("/countByCity", countByCity);
+//router.get("/countByType", getHotels);
+
+export default router;
