c89ac7bbd64f5398f3b78d9ff9cc9e37b588a33d
Arif Shahriyar Nader <arif.nader95@gmail.com>
2023-01-17 22:03:40 +0600

    first commit

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { countByCity, createHotel, deleteHotel, getHotel, getHotels, updateHotel } from '../controllers/hotel.js';
+import { countByCity, countByType, createHotel, deleteHotel, getHotel, getHotels, updateHotel } from '../controllers/hotel.js';
 
 import { createError } from '../utils/error.js';
 import { verifyAdmin } from '../utils/verifyToken.js';
@@ -19,7 +19,8 @@ router.get("/find/:id", getHotel);
 
 //GET ALL
 router.get("/", getHotels);
+
 router.get("/countByCity", countByCity);
-//router.get("/countByType", getHotels);
+router.get("/countByType", countByType);
 
 export default router;
--- client/src/hooks/useFetch.js
+++ client/src/hooks/useFetch.js
@@ -1,38 +1,37 @@
-import axios from 'axios';
-import {useEffect, useState} from 'react';
-const useFetch=(url) =>{
-    const [data, setData] =useState([])
-    const [loading, setLoading] =useState(false);
-    const [error, setError] =useState(false);
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-    useEffect(() =>{
-        const fetchData = async() =>{
-            setLoading(true)
-            try{
-                const res =await axios.get(url)
-                setData(res.data)
-            }
-            catch(err){
-                setError(err)
-            }
-            setLoading(false)
-        }
-        fetchData()
-    }, [url])
+const useFetch = (url) => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
-    const reFetch = async() =>{
-        setLoading(true)
-        try{
-            const res =await axios.get(url)
-            setData(res.data)
-        }
-        catch(err){
-            setError(err)
-        }
-        setLoading(false)
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const res = await axios.get(url);
+        setData(res.data);
+      } catch (err) {
+        setError(err);
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, [url]);
+
+  const reFetch = async () => {
+    setLoading(true);
+    try {
+      const res = await axios.get(url);
+      setData(res.data);
+    } catch (err) {
+      setError(err);
     }
-    return {data,loading,error, reFetch};
+    setLoading(false);
+  };
 
-}
+  return { data, loading, error, reFetch };
+};
 
 export default useFetch;
